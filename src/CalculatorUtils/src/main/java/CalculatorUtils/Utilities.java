4a6dde0f3a622545fa2ce1d47ff704b517cbc71e
Tomáš Tomala <tomala1omas@gmail.com>
2020-03-24 02:27:13 +0100

    Added test for method add

--- src/CalculatorUtils/src/main/java/CalculatorUtils/Utilities.java
+++ src/CalculatorUtils/src/main/java/CalculatorUtils/Utilities.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CalculatorUtils;
 
 /**
@@ -12,6 +7,14 @@ package CalculatorUtils;
 public class Utilities {
     public int add(int a, int b){
         return a+b;
+    }
+
+    public double add(double d, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public double add(double d, double d0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
--- src/CalculatorUtils/src/test/java/UtilitiesTest.java
+++ src/CalculatorUtils/src/test/java/UtilitiesTest.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+import CalculatorUtils.Utilities;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,9 +11,14 @@ import static org.junit.jupiter.api.Assertions.*;
  * @authors tomalatomas     (xtomal02)
  *          Martin Mlýnek   (xmlyne06) 
  */
+
 public class UtilitiesTest {
+    private final static double EPSILON = 0.000001;
+
+    Utilities utils;
     
     public UtilitiesTest() {
+        utils = new Utilities();
     }
     
     @BeforeAll
@@ -40,6 +40,28 @@ public class UtilitiesTest {
     
     @Test
     public void testAdd() {
-
+        //Equals
+        assertEquals(42, utils.add(42,0));
+        assertEquals(42, utils.add(21,21));
+        assertEquals(-42, utils.add(-21,-21));
+        assertEquals(-42, utils.add(-21,-21));
+        assertEquals(-543, utils.add(957,-1500));
+        assertEquals(0, utils.add(0,0));
+        assertEquals(-1, utils.add(-1,0));
+        assertEquals(109283, utils.add(84759,24524));
+        assertEquals(-109283, utils.add(-84759,-24524));
+        assertEquals(-60235, utils.add(-84759,24524));
+        assertEquals(0.05, utils.add(0.05,0),EPSILON);
+        assertEquals(8.95, utils.add(-0.05,9),EPSILON);
+        assertEquals(0.15, utils.add(0.05,0.1),EPSILON);
+        assertEquals(-0.05, utils.add(0.05,-0.1),EPSILON);
+        assertEquals(238.1195, utils.add(6.423,-244.5425),EPSILON);
+        assertEquals(0, utils.add(244.5425,-244.5425),EPSILON);
+        //Doesnt equal
+        assertFalse(utils.add(0, 0)==1);
+        assertFalse(utils.add(0, -1)==1);
+        assertFalse(utils.add(-1, 1)==1);
+        assertFalse(utils.add(-1, -1)==2);
+        assertFalse(109283==utils.add(-84759,-24524));        
     }
 }
