1714aba814a0e2dc8a09d458ce2b8e487202d5af
bobcikprogramming <62337490+bobcikprogramming@users.noreply.github.com>
2020-03-31 14:50:09 +0200

    Add files via upload
    Math library

---
+++ src/Math/MathLibrary.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathlibrary;
+
+/**
+ *
+ * @author Pavel Bobčík, xbobci03
+ * @author Tomáš Daněk, xdanek23
+ */
+public class MathLibrary {
+    
+    public double add(double first, double second) throws Exception{
+        double result = first + second; 
+
+        if(result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY){
+            throw new Exception("Infinity value");
+        }
+        
+        return result;
+    }
+    
+    public int add(int first, int second) throws Exception{
+        int result = first + second; 
+
+        if(first > 0 && second > 0 && result < 0){
+            throw new Exception("Value overflow");
+        }else if(first < 0 && second < 0 && result > 0){
+            throw new Exception("Value underflow");
+        }
+        
+        return result;
+    }
+    
+    public double sub(double first, double second) throws Exception{
+        double result = first - second; 
+
+        if(result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY){
+            throw new Exception("Infinity value");
+        }
+        
+        return result;
+    }
+    
+    public int sub(int first, int second) throws Exception{
+        int result = first - second; 
+
+        if(first > 0 && second < 0 && result < 0){
+            throw new Exception("Value overflow");
+        }else if(first < 0 && second > 0 && result > 0){
+            throw new Exception("Value underflow");
+        }
+        
+        return result;
+    }
+    
+    public double multiply(double first, double second) throws Exception{
+        double result = first * second; 
+
+        if(result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY){
+            throw new Exception("Infinity value");
+        }
+        
+        return result;
+    }
+    
+    public int multiply(int first, int second) throws Exception{
+        int result = first * second; 
+
+        if(first > 0 && second > 0 && result < 0){
+            throw new Exception("Value overflow");
+        }else if(first < 0 && second < 0 && result < 0){
+            throw new Exception("Value overflow");
+        }else if(first < 0 && second > 0 && result > 0){
+            throw new Exception("Value underflow");
+        }else if(first > 0 && second < 0 && result > 0){
+            throw new Exception("Value underflow");
+        }
+        
+        return result;
+    }
+    
+    public double divide(double first, double second) throws Exception{  
+        if(second == 0.0){
+            throw new Exception("Dividing by zero value");
+        }
+        
+        double result = first / second;
+        
+        if(result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY){
+            throw new Exception("Infinity value");
+        }
+        
+        return result;
+    }
+    
+    public int fact(int first) throws Exception{
+        if(first < 0){
+            throw new Exception("Value under zero");
+        }else if(first > 12){
+            throw new Exception("Value overflow");
+        }
+        
+        int result = 1;
+        for(int i = first; i > 1; i--){
+            result = result * i;
+        }
+        
+        return result;
+    }
+    
+    public double exp(double first, int second) throws Exception{
+        double result = 1;
+        
+        if(second == 0){
+            if(first > 0){
+                return result;
+            }else{
+                return result;
+            }
+        }else if(second < 0){
+            throw new Exception("Not supported");
+        }
+        
+        for(int i = second; i > 0; i--){
+            result = result * first;
+        }
+        
+        if(result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY){
+            throw new Exception("Infinity value");
+        }
+        
+        return result;
+    }
+    
+    public int exp(int first, int second) throws Exception{
+        int result = 1;
+        
+        if(second == 0){
+            if(first > 0){
+                return result;
+            }else{
+                return result;
+            }
+        }else if(second < 0){
+            throw new Exception("Not supported");
+        }
+        
+        int tmpPrevStep=first;
+        int tmpDivide=1;
+        for(int i = second; i > 0; i--){
+            result = result * first;
+            if((result / tmpDivide) != tmpPrevStep){
+                throw new Exception("Value overflow");
+            }
+            tmpDivide=first;
+            tmpPrevStep = result;
+        }
+        
+        return result;
+    }
+    
+    /*public double radical(double first, double second){
+        
+        
+        
+        return result;
+    }*/
+    
+    public double abs(double first){
+        return -(first);
+    }
+    
+    public int abs(int first){
+        return -(first);
+    }
+}
